5a9d9069dd379c2899fc48f1a036d3f43d3fa465
johnnydrama92 <james.ferrier01@gmail.com>
2018-10-12 00:02:03 +0100

    news api work started

--- counter/models.py
+++ counter/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Word(models.Model):
+    hash = models.CharField(max_length=200)
     word = models.CharField(max_length=200)
     count = models.IntegerField()
 
--- counter/views.py
+++ counter/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Word
 from django.template import Context, Template
+from newsapi import NewsApiClient
+import hashlib
 
 
 def dashboard(request):
@@ -9,4 +11,19 @@ def dashboard(request):
     x.sort(key=lambda a: a[1])
     x.reverse()
     d = {w:n for w,n in enumerate(x, 1)}
+    update()
     return render(request, 'counter/dashboard.html', {'words': d})
+
+def update():
+    currnet_words = Word.objects.all()
+    api = NewsApiClient(api_key='1f7851a84b9d499797e5518bee76b285')
+    bbc = api.get_top_headlines(sources='bbc-news')['articles']
+    for x in bbc:
+        data = {hashlib.md5(str.encode(x['title'])+str.encode(x['publishedAt'])).hexdigest():
+        x['description'].split()}
+        #print(data)
+        for wordset in data.values():
+            for word in wordset:
+                #print(word)
+                if Word.objects.filter(word=word).exists():
+                    print(currnet_words.word)
