b6ebe836ec4767c01c62bd0ea20d6e239cc1234a
johnnydrama92 <james.ferrier01@gmail.com>
2018-10-13 13:57:22 +0100

    postgres migration

--- counter/models.py
+++ counter/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
+
 class Word(models.Model):
-    hash = models.CharField(max_length=200)
     word = models.CharField(max_length=200)
-    number_of = models.IntegerField()
+    number_of = models.IntegerField(default=1)
+
 
     def __str__(self):
         return self.word
+
+
+class Hash(models.Model):
+    word = models.ForeignKey(Word, on_delete=models.CASCADE)
+    hash = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.hash
--- counter/views.py
+++ counter/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Word
+from .models import Word, Hash
 from django.template import Context, Template
 from newsapi import NewsApiClient
 import hashlib
@@ -15,15 +15,32 @@ def dashboard(request):
     return render(request, 'counter/dashboard.html', {'words': d})
 
 def update():
-    current_words = Word.objects.all()
+    current_words = Word()
     api = NewsApiClient(api_key='1f7851a84b9d499797e5518bee76b285')
     bbc = api.get_top_headlines(sources='bbc-news')['articles']
     for x in bbc:
         data = {hashlib.md5(str.encode(x['title'])+str.encode(x['publishedAt'])).hexdigest():
         x['description'].split()}
-        for wordset in data.values():
-            print(data.values())
+        for key in data:
+            for word in data[key]:
+                #print(value, key)
+                if Hash.objects.filter(hash=key).exists():
+                    print("yes")
+                elif Word.objects.filter(word=word).exists():
+                    print(current_words.number_of)
+                else:
+                    current_words.pk = None
+                    current_words.word = word
+                    current_words.save()
+
+
+
+
+
+
+"""
             for word in wordset:
                 if Word.objects.filter(word=word).exists():
                     for item in Word.objects.filter(word=word):
-                        print(item.word, item.number_of, item.hash, word)
+                        print(item.word, item.number_of, word)
+"""
