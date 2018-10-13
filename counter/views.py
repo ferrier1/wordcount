9b0ba6cd74d2203636e5609d6e9af4fb4c7cd777
johnnydrama92 <james.ferrier01@gmail.com>
2018-10-13 12:10:31 +0100

    api changes

--- counter/views.py
+++ counter/views.py
@@ -7,7 +7,7 @@ import hashlib
 
 def dashboard(request):
     words = Word.objects.all()
-    x = [(word.word, word.count) for word in words]
+    x = [(word.word, word.number_of) for word in words]
     x.sort(key=lambda a: a[1])
     x.reverse()
     d = {w:n for w,n in enumerate(x, 1)}
@@ -15,15 +15,15 @@ def dashboard(request):
     return render(request, 'counter/dashboard.html', {'words': d})
 
 def update():
-    currnet_words = Word.objects.all()
+    current_words = Word.objects.all()
     api = NewsApiClient(api_key='1f7851a84b9d499797e5518bee76b285')
     bbc = api.get_top_headlines(sources='bbc-news')['articles']
     for x in bbc:
         data = {hashlib.md5(str.encode(x['title'])+str.encode(x['publishedAt'])).hexdigest():
         x['description'].split()}
-        #print(data)
         for wordset in data.values():
+            print(data.values())
             for word in wordset:
-                #print(word)
                 if Word.objects.filter(word=word).exists():
-                    print(currnet_words.word)
+                    for item in Word.objects.filter(word=word):
+                        print(item.word, item.number_of, item.hash, word)
