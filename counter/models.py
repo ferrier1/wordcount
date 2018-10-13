1c565c43e91b35a14284889f2139d6565484a767
johnnydrama92 <james.ferrier01@gmail.com>
2018-10-13 09:28:24 +0100

    count-to-numberof

--- counter/models.py
+++ counter/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Word(models.Model):
     hash = models.CharField(max_length=200)
     word = models.CharField(max_length=200)
-    count = models.IntegerField()
+    number_of = models.IntegerField()
 
     def __str__(self):
         return self.word
